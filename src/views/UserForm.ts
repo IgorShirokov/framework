07e64a4e3f16599cdb6639400b8d29eae7d9f52b
Igor Shirokov <shirokov-iv@mail.ru>
2019-11-01 18:27:44 +0500

    defining an events map

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,11 +1,21 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('hi there');
+  }
+
   template(): string {
     return `
-      <div>
-        <h1>User Form</h1>
-        <input/>
+      <div class="container mt-5">
+        <h1 class="text-center mb-3">User Form</h1>
+        <input class="form-control"/>
       </div>
     `;
   }
