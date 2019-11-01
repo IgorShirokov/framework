1d6c07ce48d5c3f554a9792408c6232dddcd3c96
Igor Shirokov <shirokov-iv@mail.ru>
2019-11-02 03:21:43 +0500

    saving data from a view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,9 +5,14 @@ export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetNameClick
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -24,18 +29,14 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
       <div class="container mt-5">
-        <h1 class="text-center mb-3">User Form</h1>
-        <div class="form-group">        
-          <span class="ml-2 font-weight-bold">USER NAME:</span>
-          <span class="ml-2">${this.model.get('name')}</span>
-          <span class="ml-2 font-weight-bold">USER AGE:</span>
-          <span class="ml-2"> ${this.model.get('age')}</span>
-        </div>
-        <div class="form-group">
-          <input type="text" class="form-control"/>
+        <div class="mb-4">
+          <input type="text" class="form-control" placeholder=${this.model.get(
+            'name'
+          )} />
         </div>
         <button class="btn btn-primary set-name">Change Name</button>
         <button class="btn btn-primary set-age">Set Random Age</button>
+        <button class="btn btn-primary save-model">Save User</button>
       </div>
     `;
   }
