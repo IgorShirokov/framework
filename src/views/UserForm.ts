80a42caba5a84447f18eca5c06b412c6d92d38de
Igor Shirokov <shirokov-iv@mail.ru>
2019-11-01 18:45:19 +0500

    binding event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -16,13 +16,27 @@ export class UserForm {
       <div class="container mt-5">
         <h1 class="text-center mb-3">User Form</h1>
         <input class="form-control"/>
+        <button>Click Me</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
