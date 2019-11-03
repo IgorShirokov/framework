771a1178396de44b41a35276114fd42693bb5eb1
Igor Shirokov <shirokov-iv@mail.ru>
2019-11-03 10:11:51 +0500

    view nesting

--- core/views/UserEdit.ts
+++ core/views/UserEdit.ts
@@ -1,16 +1,24 @@
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
 import { View } from './View';
 import { User, UserProps } from '../models/User';
 
 export class UserEdit extends View<User, UserProps> {
-  regionsMap(): {[key: string]: string} {
+  regionsMap(): { [key: string]: string } {
     return {
       userShow: '.user-show',
       userForm: '.user-form'
-    }
+    };
   }
+
+  onRender(): void {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
+  }
+
   template(): string {
     return `
-      <div>
+      <div class="container mt-5">
         <div class="user-show"></div>
         <div class="user-form"></div>
       </div>
